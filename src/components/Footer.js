d493a726bb0454ea6715520ed910de309674ac46
Dionisius Jordi <dionnando01@gmail.com>
2023-02-26 20:26:46 +0700

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from '../assets/img/instagram.png';
-import navIcon2 from '../assets/img/facebook.png';
-import navIcon3 from '../assets/img/twitch.png';
+
 
 export const Footer = () => {
   return (
